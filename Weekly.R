01ed828d5264b68e7345f1cf535c604150179702
theCraver86 <128658507+theCraver86@users.noreply.github.com>
2023-10-04 17:45:23 +0200

    Add files via upload
    Added limit = 1000 to extract segment

--- Weekly.R
+++ Weekly.R
@@ -35,7 +35,6 @@ extract_df <- function(date, dimensions, metrics, top, search){
   
 }
 
-
 extractMultipleWeek <- function(lastSunday, nWeek, dimensions, metrics, top, search){
   
   output = "";
@@ -58,30 +57,38 @@ extractMultipleWeek <- function(lastSunday, nWeek, dimensions, metrics, top, sea
 
 ### - Login & Utilities
 
-#set_config(use_proxy("proxy.user.alitalia.local", port = 8080, username = "IT011820", password = "Agosto23", auth = "basic"))
+set_config(use_proxy("proxy.user.alitalia.local", port = 8080, username = "IT011820", password = "Agosto23", auth = "basic"))
 
 # Login JWT
 aw_auth_with('jwt')
 aw_auth()
 
-
-### Login OAUTH
-#aw_auth_with('oauth')
-
 # dimension = data.frame(aw_get_dimensions()) #to get a list of available dimensions IDs.
 # metric = data.frame(aw_get_metrics())  #to get a list of available metrics IDs.
 # calculatedmetrics = data.frame(aw_get_calculatedmetrics()) #to get a list of available calculated metrics IDs.
-# segment = aw_get_segments()
+# segment = data.frame(aw_get_segments(limit = 1000))
 
 ### - Setup Default Variable
 
 
 ### - E_xtract
 lastSunday = '2023-10-01';
-nWeek = 2;
+nWeek = 4;
 
-nCategory = 3;
+nCategory = 30;
 nPrimaryCategory = 3;
+
+dimensions <- c("category","prop17");
+metrics <- c("revenue", "orders");
+top = c(nCategory, nPrimaryCategory);
+search = c("", "MATCH 'Booking' OR 'Checkin' OR 'Rebooking'");
+
+ff__test <- extractMultipleWeek(lastSunday, nWeek, dimensions, metrics, top, search) 
+
+write_xlsx(ff__test, "C:/Users/IT011820/OneDrive - ITA Italia Trasporto Aereo/Desktop/0. R/02_exported/ff__test.xlsx")
+
+get_me()
+aw_get_reportsuites(company_id = 'ITASPA')
 
 dimensions <- c("category","prop17");
 metrics <- c("revenue", "orders");
