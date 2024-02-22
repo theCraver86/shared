8b5592ccce1ee2c75a0157b0e4e075785eccea39
theCraver86 <128658507+theCraver86@users.noreply.github.com>
2024-02-22 02:14:58 +0100

    Kaggle first attempt

--- titanic_project.R
+++ titanic_project.R
@@ -8,11 +8,25 @@ library(neuralnet)
 
 #install_formats()
 
-#test <- import("C:/Users/rtadd/OneDrive/Desktop/R/3titanic/0_import/test.csv")
+test <- import("C:/Users/rtadd/OneDrive/Desktop/R/3titanic/0_import/test.csv")
 train <- import("C:/Users/rtadd/OneDrive/Desktop/R/3titanic/0_import/train.csv")
 #gender_submission <- import("C:/Users/rtadd/OneDrive/Desktop/R/3titanic/0_import/gender_submission.csv")
 
 str(train)
+
+test_numeric <- test %>%
+  mutate(
+    Sex_reco = ifelse(Sex == 'male', 1 , 2),
+    Embarked_reco = case_when(
+      Embarked %in% "C" ~ 1,
+      Embarked %in% "Q" ~ 2,
+      Embarked == "S" ~ 3
+    ) 
+  ) %>% 
+  mutate(Age =ifelse(is.na(Age) == TRUE, mean(Age, na.rm = TRUE), Age)) %>%  
+  mutate(Embarked_reco = ifelse(is.na(Embarked_reco) == TRUE, 3, Embarked_reco)) %>% 
+  select(-c(Name, Sex, Embarked, Cabin, Ticket)) %>% 
+  mutate(Fare =ifelse(is.na(Fare) == TRUE, mean(Fare, na.rm = TRUE), Fare))
 
 train_numeric <- train %>%
   mutate(
@@ -25,7 +39,8 @@ train_numeric <- train %>%
   ) %>% 
   mutate(Age =ifelse(is.na(Age) == TRUE, mean(Age, na.rm = TRUE), Age)) %>%  
   mutate(Embarked_reco = ifelse(is.na(Embarked_reco) == TRUE, 3, Embarked_reco)) %>% 
-  select(-c(Name, Sex, Embarked, Cabin, Ticket))
+  select(-c(Name, Sex, Embarked, Cabin, Ticket)) 
+#  mutate_if(is.numeric, funs(as.numeric(scale(.))))
 
 res <- cor(train_numeric) 
 res <- round(res, 2)
@@ -38,9 +53,34 @@ model <- lm(Survived ~ Age + Sex_reco + Pclass, data = train_numeric)
 summary(model)
 summary(model)$coefficients
 
-log_model <- glm(Survived ~ Age + Sex_reco + Pclass + SibSp + Parch + Fare + Embarked_reco, data = train_numeric, family = "binomial")
+log_model <- glm(Survived ~ 
+                  #Sex_reco + Pclass,
+                 Age + Sex_reco + Pclass + SibSp + Embarked_reco,
+                data = train_numeric, family = "binomial")
+                #data = train_numeric)
 summary(log_model)
 summary(log_model)$coefficients
+
+chi <- log_model[["null.deviance"]] - log_model[["deviance"]]
+
+post_logit <- predict(log_model, type = "response")
+head(post_logit)
+
+default_logit <- ifelse(post_logit>0.5, "PreV: 1", "Prev: 0")
+calc <- table(default_logit, train_numeric$Survived)
+
+perc <- round(((calc[1,1] + calc[2,2])  / (calc[1,1] + calc[1,2] + calc[2,1] + calc[2,2])*100),2)
+perc
+
+### Output
+output_post_logit <- predict(log_model, 
+                             newdata = test_numeric, se.fit = TRUE)
+#newdata = temperature = c(70, 90), se.fit = TRUE)
+head(output_post_logit)
+
+output <- ifelse(output_post_logit$fit>0.5, 1, 0)
+
+write_xlsx(data.frame(output), "C:/Users/rtadd/OneDrive/Desktop/R/3titanic/0_export/output.xlsx")
 
 
 str(train_numeric)
